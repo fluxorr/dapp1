8b4df8dea33f04dc6f5c1da1b4e5969743544363
Rahul Chaudhari_ <rahulgc545@gmail.com>
2025-04-27 19:55:47 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-white flex justify-center items-center h-screen  ' >
+        Class 5.1
+      </div>
+    </>
+  )
+}
+
+export default App
